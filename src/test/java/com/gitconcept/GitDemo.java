2ff2d7f65d94d8807e7068b1ebb675a10cc43cd6
SAGAR ALASKAR <SAGAR ALASKAR@DESKTOP-NL9CC06>
2021-11-16 12:07:25 +0530

    first version of project

---
+++ src/test/java/com/gitconcept/GitDemo.java
@@ -0,0 +1,75 @@
+package com.gitconcept;
+
+public class GitDemo {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * 
+		 * Agenda
+		 * -----------
+		 * what is git?
+		 * what is github?
+		 * how to work with git and github?
+		 * commit
+		 * Push
+		 * Pull
+		 * Branching
+		 * Raising Pull Request(PR)
+		 * Approve Pull Request(Pr)
+		 * Merging
+		 * 
+		 * 
+		 * 
+		 * Normally multiple people working on multiple things.
+		 * 1st guy doing some activity
+		 * 2nd doing some
+		 * and so-on
+		 * they working as one team working on same project
+		 * 
+		 * so at the end of the day we need to integrate all the task and we need to keep the task 
+		 * at the common place.
+		 * So for this we need to use configuration management or Version control system
+		 * So here Git and Github are used 
+		 * for ex- git is an local repository 
+		 * and GitHub is an our Remote repository.
+		 * means git is in Install on your local repository.
+		 * and everybody in the team they can maitain the code version in this git local repository.
+		 * And end of the day they have to push their code in remote repository
+		 * Basiclly github is a server that perticular server can be used by using the git software
+		 * 
+		 * Git- Git is a version control system used to track changes in computer files.Its a tool to 
+		 * manage your code and file history while coordinating work remotely on those file with others.
+		 * 
+		 * sometimes what happen there are multiple people work multiple think and team lead he is main
+		 * responsible guys to delever the project ,so what the team lead do it he will not allow to
+		 * everybody to checking pushing the code directly into a remote repository.
+		 * team lead just to inform to everybody of team member to commit there code into local branches
+		 * and to review all the code team lead.Lead reviev that perticular code and once they give approval
+		 * that code then team member merge that code into remote repository.
+		 * 
+		 * So there is one repository are there in github that is called as master Branch.
+		 * 
+		 * Github-  Github is a hosting service for git repositories. Git is the tool,while github is the
+		 *          service to use git.  But in this server site threre are some chances to get confilct.
+		 *          
+		 * 
+		 * Braching
+		 * Merging
+		 * 
+		 * this are the two main concept.
+		 * 
+		 * 
+		 * Cloning - Everybody in the team will get perticular one copy of that project in there
+		 *           local system.And they can perform the task on that project in their local system.
+		 * 
+		 * 
+		 * 
+		 */
+		
+		   
+		
+
+	}
+
+} 
